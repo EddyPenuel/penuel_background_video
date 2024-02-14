44ae1adf8ca77b9a8b27ea9109ac83ed0b0d19ff
eddy2024 <eddysoftwarengineer@gmail.com>
2024-02-14 18:43:15 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+const btn = document.querySelector(".btn");
+const video = document.querySelector(".background-video");
+const fa = document.querySelector(".fa");
+const preloader = document.querySelector(".preloader") ;
+
+btn.addEventListener()
+
+window.addEventListener("load", () => {
+    preloader.computedStyleMap.zIndex = " -999";
+});
